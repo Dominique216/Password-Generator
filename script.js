dc03b2f728104be3b48e1f13bd3f223471b623e0
Dominique Nix <dominiquenix@Dominiques-MacBook-Air.local>
2022-12-22 23:42:33 -0600

    added inital propmt and user choices

--- script.js
+++ script.js
@@ -33,4 +33,66 @@ function generatePassword() {
     //return passwordString
 //need a forloop somewhere 
 //lots of if statement//
+  var passwordLength = window.prompt('Choose a password length between 8 and 128 characters', '');
+
+ //Checks for only numbers
+    numOnly = /\d+/
+    passwordLength = passwordLength.match(numOnly);
+
+  //Checks for valid length
+   if (passwordLength < 8 || passwordLength > 128) { 
+    window.alert('Choose vaid input. Try Again')
+    return null;
+   } 
+
+   //Checks for only numbers
+  if (passwordLength == /\d+/) {
+    window.alert('Choose vaid input. Try Again')
+    return null
+  }
+
+ //variables to keep track of what the user wants in their password
+  var upperCase;
+  var lowerCase;
+  var numbers;
+  var special;
+
+//asks if the user wants uppercase letters, then set the value of uppercase to either true or false
+  var upperCaseQ = confirm('Do you want to include UPPERCASE letters?  OK for yes/ CANCEL for no')
+   if(upperCaseQ) {
+    upperCase = true;
+   } else {
+    upperCase= false;
+   }
+
+//asks if the user wants lowercase letters, then set the value of lowercase to either true or false
+  var lowerCaseQ = confirm('Do you want to include LOWERCASE letters? OK for yes/ CANCEL for no')
+  if(lowerCaseQ) {
+    lowerCase = true;
+   } else {
+    lowerCase= false;
+   }
+
+//asks if the user wants numbers, then set the value of numbers to either true or false
+  var numbersQ= confirm('Do you want to include NUMBERS letters? OK for yes/ CANCEL for no')
+  if(numbersQ) {
+    numbers = true;
+   } else {
+    numbers = false;
+   }
+
+//asks if the user wants SPECIAL CHARACTERS letters, then set the value of SPECIAL CHARACTERS to either true or false
+  var specialQ = confirm('Do you want to include SPECIAL CHARACTERS letters? OK for yes/ CANCEL for no')
+  if(specialQ) {
+    special = true;
+   } else {
+    special= false;
+   }
+  
+   //keeping track to see if code is working properly
+  console.log(upperCase);
+  console.log(lowerCase);
+  console.log(numbers);
+  console.log(special);
+
 }
